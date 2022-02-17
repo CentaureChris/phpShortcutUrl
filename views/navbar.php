07ade2beff78f69ef839fd6caa58112d05b0e4b2
CentaureChris <c.centaure972@gmail.com>
2022-02-18 00:16:33 +0100

    add some css

--- views/navbar.php
+++ views/navbar.php
@@ -1,11 +1,11 @@
 <nav>
-    <ul>
+    <ul class="admin">
+        <li><a href="index.php">Url shortcut</a></li>
         <?php 
         if(!isset($_SESSION['Auth'])): ?>
         <li><a href="index.php?page=login">Login</a></li>
         <?php else : ?>
         <li><a href="index.php?page=logout">Logout</a></li>
         <?php endif ; ?>
-        <li><a href="index.php">Url shortcut</a></li>
     </ul>
 </nav>
